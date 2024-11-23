b22e11bcc3c45513339db3ee6076cbd120eb12b5
anim200 <126519717+anim200@users.noreply.github.com>
2024-11-23 15:32:25 +0600

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,12 @@ app.get("/", (req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    connect();
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  await connect(); // Ensure database connection is established
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+};
+
+startServer();
+
